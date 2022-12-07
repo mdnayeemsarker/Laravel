f54310d2699a0782afaa3c31838f5ebbc21f403f
Nayeemasha703 <nayeemasha703@gmail.com>
2022-12-07 15:03:33 +0600

    Modify UI of welcome page

---
+++ resources/views/frontend/docs.blade.php
@@ -0,0 +1,2 @@
+@extends('frontend.layouts.app')
+@section('titile') Docs @endsection 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+//Route
+Route::get('/docs', function () {
+    return view('frontend.docs');
 });

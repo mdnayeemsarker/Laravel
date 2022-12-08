075e0961f9685a1ce1bd114d3e7df0bee59dfddc
Nayeemasha703 <nayeemasha703@gmail.com>
2022-12-08 18:22:31 +0600

    Customize portfolio template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
         Personal Website - Code With Dary
     </title>
-    <link
-        rel="stylesheet"
-        href="style.css"
-    />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&display=swap"
-        rel="stylesheet"
-    />
-    <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.0.7/css/all.css"
-    />
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" />
+    <style>
+        .active {
+            color: red;
+        }
+    </style>
 </head>
 
 <body>
 
     <!-- header -->
-    @include('layouts.partials.header')
+    <header>
+        @include('layouts.partials.header')
+    </header>
 
     @yield('content')
 
     <!-- Footer -->
     @include('layouts.partials.footer')
-    
+
 </body>
+
 </html>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,26 +1,24 @@
-<header>
-    <!-- Navigation -->
-    <nav class="top-menu-container">
-        <div class="logo-header">
-            <a href="">
-                <img
-                src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png"
-                alt="Logo personal portfolio"
-                title="Logo personal portfolio"
-                />
-            </a>
-        </div>
+<!-- Navigation -->
+<nav class="top-menu-container">
+    <div class="logo-header">
+        <a href="">
+            <img src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png"
+                alt="Logo personal portfolio" title="Logo personal portfolio" />
+        </a>
+    </div>
 
-        <ul>
-            <li>
-                <a href="">About</a>
-            </li>
-            <li>
-                <a href="">Portfolio</a>
-            </li>
-            <li>
-                <a href="">Contact</a>
-            </li>
-        </ul>
-    </nav>
-</header>
+    <ul>
+        <li>
+            <a href="/" class="{{ request()->is('/') ? 'active' : '' }}">Home</a>
+        </li>
+        <li>
+            <a href="about" class="{{ request()->is('about') ? 'active' : '' }}">About</a>
+        </li>
+        <li>
+            <a href="portfolio" class="{{ request()->is('portfolio') ? 'active' : '' }}">Portfolio</a>
+        </li>
+        <li>
+            <a href="contact" class="{{ request()->is('contact') ? 'active' : '' }}">Contact</a>
+        </li>
+    </ul>
+</nav>

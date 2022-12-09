276c7b1a668e999685316438b0f1aeafd1b616bd
Nayeemasha703 <nayeemasha703@gmail.com>
2022-12-09 13:16:48 +0600

    Learng Query builder

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts/index');
+        $posts = DB::table('posts')
+            ->get();
+        dd($posts);
     }
 }
